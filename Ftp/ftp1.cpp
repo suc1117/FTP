009b616d9324ba6951b3b289a77d0e6e4b7c2183
suc1117 <suc1117@naver.com>
2019-11-12 16:40:26 +0900

    add CloseFtp()
    arrange code about server

--- Ftp/ftp1.cpp
+++ Ftp/ftp1.cpp
@@ -1,60 +1,75 @@
 #include <Windows.h>
 #include <wininet.h> 
 #include <iostream>
-
 #pragma comment(lib, "Wininet")
-using namespace std;
 
-const char* SERVER_ADDRESS="ip_address";
-const char* UID = "id";
-const char* PASS = "pw";
+/// FTP SERVER SETTINGS ///
+const char* SERVER_ADDRESS="SERVER_ADDRESS";
+const char* ID = "ID";
+const char* PW = "PASSWORD";
 
-string StdPath = "./"; // Upload/Download Path
+std::string RemotePath = "./"; // Upload/Download Path
 HINTERNET hInternet;
 HINTERNET hFtpSession;
 int SERVER_STATUS=0; // 0= Offline 1=Online
 
-int ConnectFtp()
+void ConnectFtp()
 {
 	hInternet = InternetOpen(NULL, INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, 0);
 	if (hInternet == NULL)
 	{
-		cout << "Error: InternetOpen = " << GetLastError() << endl;
-		return 0;
+		std::cout << "Internet Open Error: " << GetLastError() << std::endl;
+		return;
 	}
 	else
 	{
-		hFtpSession = InternetConnect(hInternet, SERVER_ADDRESS, INTERNET_DEFAULT_FTP_PORT, UID, PASS, INTERNET_SERVICE_FTP, 0, 0);
+		hFtpSession = InternetConnect(hInternet, SERVER_ADDRESS, INTERNET_DEFAULT_FTP_PORT, ID, PW, INTERNET_SERVICE_FTP, 0, 0);
 		if (!hFtpSession)
 		{
-			cout << "Error: InternetConnect = " << GetLastError() << endl;
-			return 0;
+			std::cout << "Internet Connect Error: " << GetLastError() << std::endl;
+			return;
 		}
 		else 
 		{
-			cout << "Server Connected..." << endl;
-			SERVER_STATUS= 1;
+			SERVER_STATUS = 1;
+			std::cout << "Server Connected..." << std::endl;
+			std::cout << "SERVER_ADDRESS : " << SERVER_ADDRESS << std::endl;
+			std::cout << "SERVER_STATUS : "<< SERVER_STATUS << std::endl;
 		}
 	}
-	return 1;
+	return;
+}
+
+void CloseFtp()
+{
+	if (hInternet)
+	{
+		InternetCloseHandle(hInternet);
+		hInternet = NULL;
+	}
+
+	if (hFtpSession)
+	{
+		InternetCloseHandle(hFtpSession);
+		hFtpSession = NULL;
+	}
+	std::cout << "Connection Closed..."<< std::endl;
 }
 
 void FileSubmit(char* Destfilename) // Upload File to FTP Server
 {
 	if (SERVER_STATUS)
 	{
-		string tDestFile = StdPath + Destfilename;
+		std::string tDestFile = RemotePath + Destfilename;
 		const char* DestPath = tDestFile.c_str();
-		if (!FtpPutFile(hFtpSession, DestPath, Destfilename, FTP_TRANSFER_TYPE_BINARY, 0)) // ���� ����, ���� �̸�
+		if (!FtpPutFile(hFtpSession, DestPath, Destfilename, FTP_TRANSFER_TYPE_BINARY, 0))
 		{
-			cout << "Error: FtpPutFile = " << GetLastError() << endl; 
+			std::cout << "File Upload Error: " << GetLastError() << std::endl;
 		}
 		else 
 		{
-			cout << "File Upload Complete" << endl;
-			InternetCloseHandle(hFtpSession);
+			std::cout << "File Upload Complete" << std::endl;
 		}
-		InternetCloseHandle(hInternet);
 	}
 }
 
@@ -62,19 +77,17 @@ void FileDown(char* Destfilename) // Download File from FTP Server
 {
 	if (SERVER_STATUS)
 	{
-		string tDestFile = StdPath + Destfilename;
+		std::string tDestFile = RemotePath + Destfilename;
 		const char* DestPath = tDestFile.c_str();
 		printf("Download Path : %s\n", DestPath);
 		if (!FtpGetFile(hFtpSession, DestPath, Destfilename, FALSE, NULL, FTP_TRANSFER_TYPE_BINARY, NULL))
 		{
-			cout << "Error: " << GetLastError();
+			std::cout << "File Download Error: " << GetLastError();
 		}
 		else
 		{
-			cout << "File Downloaded" << endl;
-			InternetCloseHandle(hFtpSession);
+			std::cout << "File Downloaded" << std::endl;
 		}
-		InternetCloseHandle(hInternet);
 	}
 }
 
@@ -83,14 +96,12 @@ void FileRemove(char* filename) // Remove File
 	if (SERVER_STATUS)
 	{
 		if (!FtpDeleteFile(hFtpSession, filename)) {
-			cout << "Error: " << GetLastError();
+			std::cout << "File Remove Error: " << GetLastError();
 		}
 		else
 		{
-			cout << "File Removed..." << endl;
-			InternetCloseHandle(hFtpSession);
+			std::cout << "File Removed..." << std::endl;
 		}
-		InternetCloseHandle(hInternet);
 	}
 }
 
@@ -100,5 +111,6 @@ int main()
 	//FileSubmit("FTP_1105.zip"); // File Upload
 	//FileDown("FTP_1105.zip"); // File Download
 	//FileRemove("appserver1.cap");
+	CloseFtp();
 	return 0;
 }
---
+++ README.md
@@ -0,0 +1,14 @@
+# FTP
+using VC++ 2017 On Windows10
+
+11/05 
+---
+int ConnectFtp() // 서버 연결 함수
+
+void FileSubmit(char* Destfilename) // 파일 업로드
+
+void FileDown(char* Destfilename) // 파일 다운로드
+
+void FileRemove(char* filename) // 파일 삭제
+
+---
